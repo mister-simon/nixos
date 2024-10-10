f5a4080a853e8c37561810cdd8e413fc891b3a02
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-10-10 10:55:18 +0100

    Working state with home manager basics

--- flake.nix
+++ flake.nix
@@ -2,20 +2,33 @@
   description = "Simon's Very Good NixOS Flake.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    home-manager = {
+      #url = "github:nix-community/home-manager/release-24.05";
+      #inputs.nixpkgs.follows = "nixpkgs-stable";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    { self, nixpkgs, home-manager, ... }@inputs:
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
       nixosConfigurations = {
         leafsprite = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [ ./leafsprite/configuration.nix ];
+          modules = [ 
+		./leafsprite/configuration.nix
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager.users.simon = import ./leafsprite/home.nix;
+                }
+	  ];
         };
       };
     };
