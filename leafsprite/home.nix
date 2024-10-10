0f0a781895784bdf1fec76998dfc016c4c6f74fc
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-10-10 13:12:41 +0100

    Actually getting ps1 working

--- leafsprite/home.nix
+++ leafsprite/home.nix
@@ -19,6 +19,18 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.git = {
+    enable = true;
+    userName = "Simon W";
+    userEmail = "4751279+mister-simon@users.noreply.github.com";
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+
   programs.bash = {
     enable = true;
     enableCompletion = true;
@@ -36,53 +48,60 @@
       nrs = "sudo nixos-rebuild switch --flake ~/nixos/#leafsprite";
     };
     initExtra = ''
-      # ~/.bashrc: executed by bash(1) for non-login shells.
-      # If not running interactively, don't do anything
-      case $- in
-          *i*) ;;
-            *) return;;
-      esac
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# If not running interactively, don't do anything
+case $- in
+  *i*) ;;
+    *) return;;
+esac
 
-      # don't put duplicate lines or lines starting with space in the history.
-      # See bash(1) for more options
-      HISTCONTROL=ignoreboth
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
 
-      # append to the history file, don't overwrite it
-      shopt -s histappend
+# append to the history file, don't overwrite it
+shopt -s histappend
 
-      # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-      HISTSIZE=1000
-      HISTFILESIZE=2000
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
 
-      # check the window size after each command and, if necessary,
-      # update the values of LINES and COLUMNS.
-      shopt -s checkwinsize
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
 
-      # set a fancy prompt (non-color, unless we know we "want" color)
-      case "$TERM" in
-          xterm-color|*-256color) color_prompt=yes;;
-      esac
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+  xterm-color|*-256color) color_prompt=yes;;
+esac
 
-      # Alias definitions.
-      # You may want to put all your additions into a separate file like
-      # ~/.bash_aliases, instead of adding them here directly.
-      # See /usr/share/doc/bash-doc/examples in the bash-doc package.
+export PROMPT_DIRTRIM=3
 
-      if [ -f ~/.bash_aliases ]; then
-          . ~/.bash_aliases
-      fi
+source ~/.nix-profile/share/git/contrib/completion/git-prompt.sh
+
+if [ "$color_prompt" = yes ]; then
+    PS1='\[\033[38;5;239m\]\A\[$(tput sgr0)\] ''${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\w\[\033[00m\]\033[0;32m$(__git_ps1 " (%s)")\033[0m\$ '
+else
+    PS1='\A ''${debian_chroot:+($debian_chroot)} \w$(__git_ps1 " (%s)")\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm* | rxvt*)
+    PS1="\[\e]0;''${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*) ;;
+esac
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+  . ~/.bash_aliases
+fi
     '';
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Simon W";
-    userEmail = "4751279+mister-simon@users.noreply.github.com";
-  };
-
-  programs.zoxide = {
-    enable = true;
-    enableBashIntegration = true;
-    options = [ "--cmd cd" ];
   };
 }
