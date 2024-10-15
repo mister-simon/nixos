b6dfa41a9220c20258968d1d1450b1abd5945040
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-10-15 11:15:45 +0100

    Fix inputs and home manager unfree issue

--- flake.nix
+++ flake.nix
@@ -4,13 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
       #url = "github:nix-community/home-manager/release-24.05";
       #inputs.nixpkgs.follows = "nixpkgs-stable";
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -31,6 +30,8 @@
             ./leafsprite/configuration.nix
             home-manager.nixosModules.home-manager
             {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
               home-manager.users.simon = import ./leafsprite/home.nix;
             }
           ];
