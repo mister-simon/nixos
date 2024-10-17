65b49a297f2ffb2344912c7d50919d9011f3f965
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-10-17 13:08:12 +0100

    Tinkering with plymouth. Doesn't seem to kick in until after luks decryption and only for a split second so... Eh. Needs more tinkering.

--- leafsprite/configuration.nix
+++ leafsprite/configuration.nix
@@ -28,7 +28,25 @@
     initrd.verbose = false;
     kernelParams = [
       "quiet"
+      # "splash"
+      # "boot.shell_on_fail"
+      # "loglevel=3"
+      # "rd.systemd.show_status=false"
+      # "rd.udev.log_level=3"
+      # "udev.log_priority=3"
     ];
+
+    # Add plymouth to boot loader :Z
+    # plymouth = {
+    #   enable = true;
+    #   theme = "rings";
+    #   themePackages = with pkgs; [
+    #     # By default we would install all themes
+    #     (adi1090x-plymouth-themes.override {
+    #       selected_themes = [ "rings" ];
+    #     })
+    #   ];
+    # };
 
     # We should be able to read whatever fs we like...
     supportedFilesystems = [
