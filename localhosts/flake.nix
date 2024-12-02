78208f28491907771bc0e141fd928d7e2cfd7de6
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-12-02 16:09:20 +0000

    Formatting the localhosts flake

--- localhosts/flake.nix
+++ localhosts/flake.nix
@@ -1,15 +1,26 @@
 {
   description = ''
-    
-        This will store any local changes to a local hosts file.
-        Copy this to ~/localhosts to be used as an untracked flake input.
+    This will store any local changes to a local hosts file.
+    Copy this to ~/localhosts to be used as an untracked flake input.
   '';
 
-  inputs = { };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs =
-    { self }:
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
     {
+      # Formatting
+      formatter."x86_64-linux" = pkgs.nixfmt-rfc-style;
+
       hosts = {
         # "192.168.56.56" = [ "example.test" ];
       };
