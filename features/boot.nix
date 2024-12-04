e72dff153e8b7d2f25474837352a30322a320c51
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-12-04 17:19:13 +0000

    Adding filesystem support and checking out the consoleMode option. Maybe it'll make the boot menu a bit more reasonably sized?

--- features/boot.nix
+++ features/boot.nix
@@ -1,5 +1,18 @@
 { ... }:
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
+
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.supportedFilesystems = [
+    "ntfs"
+    "exfat"
+    "vfat"
+    "btrfs"
+    "reiserfs"
+    "f2fs"
+    "xfs"
+    "cifs"
+  ];
 }
