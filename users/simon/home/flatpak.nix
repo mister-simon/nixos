552cdb5bbdb20720377bd42cdcd683a1188773b9
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-06-04 19:43:28 +0100

    Rm leafsprite host files

--- users/simon/home/flatpak.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-{
-  home.sessionVariables = {
-    # lets flatpak work
-    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
-  };
-}
