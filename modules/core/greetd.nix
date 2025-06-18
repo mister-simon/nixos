c23c1ae0cf6eb9e5709e5a285578d9d521bfba8a
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-06-18 12:29:56 +0100

    Trying out autologin and additional tuigreet params.

--- modules/core/greetd.nix
+++ modules/core/greetd.nix
@@ -7,9 +7,13 @@
     enable = true;
     vt = 3;
     settings = {
+      initial_session = {
+        user = username;
+        command = "Hyprland";
+      };
       default_session = {
         user = username;
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --remember-user-session --time --cmd Hyprland"; # start Hyprland with a TUI login manager
       };
     };
   };
