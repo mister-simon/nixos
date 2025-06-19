2c5f323f192822fbe9e90fb64424c1818f9a46a4
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-06-19 12:02:15 +0100

    Adds treefmt

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -51,6 +51,7 @@
     ncdu # Disk Usage Analyzer With Ncurses Interface
     nixfmt-rfc-style # Nix Formatter
     nixd
+    nixfmt-tree
     nwg-displays # configure monitor configs via GUI
     onefetch # provides zsaneyos build info on current system
     pavucontrol # For Editing Audio Levels & Devices
