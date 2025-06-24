2febc782821fab01de8fb08e8bf78a933941cbec
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-06-24 14:51:01 +0100

    Readds php as a host package

--- hosts/zanix/host-packages.nix
+++ hosts/zanix/host-packages.nix
@@ -12,6 +12,21 @@
     protonvpn-gui
     tldr
     vscode
+
+    # PHP
+    php84
+    php84Packages.composer
+    php84Extensions.ctype
+    php84Extensions.curl
+    php84Extensions.dom
+    php84Extensions.fileinfo
+    php84Extensions.filter
+    php84Extensions.mbstring
+    php84Extensions.openssl
+    php84Extensions.pdo
+    php84Extensions.session
+    php84Extensions.tokenizer
+    php84Extensions.xml
   ];
 
   programs.zsh.enable = true;
