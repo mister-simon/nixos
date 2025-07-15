d06335a242eb628a988687a5a8271b1e36a65b56
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-07-15 15:43:33 +0100

    Enabling yazi integrations

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -7,6 +7,10 @@ in
 {
   programs.yazi = {
     enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    shellWrapperName = "yy";
     settings = settings;
     keymap = keymap;
     theme = theme;
@@ -18,11 +22,11 @@ in
     };
 
     initLua = ''
-      			require("full-border"):setup()
-            require("git"):setup()
-            require("smart-enter"):setup {
-              open_multi = true,
-            }
-      		'';
+      require("full-border"):setup()
+         require("git"):setup()
+         require("smart-enter"):setup {
+           open_multi = true,
+         }
+    '';
   };
 }
