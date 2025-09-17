81293a0a60d11a8b1af97273b6e3809fca3ac3ad
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-09-17 13:32:52 +0100

    Revert to stable branch gnome option names

--- modules/core/gnome.nix
+++ modules/core/gnome.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   # Enable the GNOME Desktop Environment.
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
   # Allow setting up online accounts
   services.gnome.gnome-online-accounts.enable = true;
